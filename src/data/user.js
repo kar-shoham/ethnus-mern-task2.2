ad461aa71d723b7546753d58a16d527e9ed004a6
Shoham <kar.shoham.25@gmail.com>
2023-06-20 09:43:02 +0530

    initial commit

---
+++ src/data/user.js
@@ -0,0 +1,84 @@
+const INFO = {
+	main: {
+		title: "Shoham Kar Portfolio",
+		name: "Shoham Kar",
+		email: "shoham.kar2020@vitstudent.ac.in",
+		logo: "https://w7.pngwing.com/pngs/250/751/png-transparent-avatar-computer-icons-man-avatar-face-heroes-logo-thumbnail.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "A part time develper and a full time binge watcher",
+		description:
+			"Hii, I am a full stack developer with expertise in the tech stack MERN(MongoDB, Express, NodeJS and ReactJS). I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I'm Shoham Kar. I live in New Delhi, India.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Task 1",
+			description:
+				"It is a responsive paragraph word counter",
+			logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm2TGd9dF5gM0esb3fqx5sCkkeLw20RRmWS2NYLoymmA&s",
+			linkText: "View Task",
+			link: "https://master--stalwart-platypus-adb4c4.netlify.app/task-1",
+		},
+
+		{
+			title: "Task 2",
+			description:
+				"It is a calculator app in react where we can perform all the basic calculations",
+			logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm2TGd9dF5gM0esb3fqx5sCkkeLw20RRmWS2NYLoymmA&s",
+			linkText: "View Task",
+			link: "https://master--stalwart-platypus-adb4c4.netlify.app/task-2",
+		},
+
+		{
+			title: "Task 3",
+			description:
+				"This color picker allows the user to pick a color from a list of colors and set the color as the background color of the button",
+			logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm2TGd9dF5gM0esb3fqx5sCkkeLw20RRmWS2NYLoymmA&s",
+			linkText: "View Task",
+			link: "https://master--stalwart-platypus-adb4c4.netlify.app/task-3",
+		},
+
+		{
+			title: "Task 4",
+			description:
+				"We fetch the user records from the api and display the user details in form of a table in react",
+			logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm2TGd9dF5gM0esb3fqx5sCkkeLw20RRmWS2NYLoymmA&s",
+			linkText: "View Task",
+			link: "https://master--stalwart-platypus-adb4c4.netlify.app/task-4",
+		},
+
+		{
+			title: "Task 5",
+			description:
+				"In this age calculator, we give our bithdate as the input and it shows us our age as of now",
+			logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm2TGd9dF5gM0esb3fqx5sCkkeLw20RRmWS2NYLoymmA&s",
+			linkText: "View Task",
+			link: "https://master--stalwart-platypus-adb4c4.netlify.app/task-5",
+		},
+	],
+};
+
+export default INFO;
